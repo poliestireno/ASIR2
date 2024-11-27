c919850644d23cc46b7bb17cbb56443fe2ebdb99
poliestireno <poliestireno@gmail.com>
2024-11-27 13:43:45 +0100

    numero oculto

--- IAW-main/Proyecto01/iaw16.php
+++ IAW-main/Proyecto01/iaw16.php
@@ -1,3 +1,40 @@
+<?php
+
+//var_export($_POST);
+
+// es la primera vez ??
+if (count($_POST) == 0) 
+{
+    $cont=0;
+    $num_oculto = rand(1,50);
+}
+else    // las siguientes veces
+{
+    $cont = $_POST["contador"];
+    $cont++;  // $cont = $cont + 1;
+
+    $num_oculto = $_POST['numeroOculto'];
+    if ($num_oculto == $_POST['numeroJugador'])
+    {
+        echo "<script>alert('EDLB con ". $cont. " intentos');</script>";
+    }
+    else if ($num_oculto > $_POST['numeroJugador'])
+    {
+        echo "<script>alert('El nº introducido es mEnor que el nº oculto');</script>";
+    }
+    else
+    {
+        echo "<script>alert('El nº introducido es mAyor que el nº oculto');</script>";
+    }
+}
+
+//echo "num ale:".$num_oculto;
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,10 +49,15 @@
         </div>
         <div>
             <input type="number" name="numeroJugador"/>
+            <input type="hidden" name="numeroOculto" value="<?php echo $num_oculto?>"/>
+            <input type="hidden" name="contador" value="<?php echo $cont?>"/>
         </div>
         <div>
             <input type="submit" name="boton" value="GO!"/>
         </div>
     </form>
+    <div>
+        <h3>Nº de intentos: <?php echo $cont?></h3>
+    </div>
 </body>
 </html>
