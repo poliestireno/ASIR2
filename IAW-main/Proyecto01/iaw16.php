fde703d9ad197c5fbe7eb839accc859552e78ef9
poliestireno <poliestireno@gmail.com>
2024-11-27 11:05:19 +0100

    numero secreto

---
+++ IAW-main/Proyecto01/iaw16.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="iaw16.php" method="post">
+        <div>
+            <label>Introduce número:</label>
+        </div>
+        <div>
+            <input type="number" name="numeroJugador"/>
+        </div>
+        <div>
+            <input type="submit" name="boton" value="GO!"/>
+        </div>
+    </form>
+</body>
+</html>
